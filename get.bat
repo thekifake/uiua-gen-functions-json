217dd22bc72da31e92130b7eff657478d1fa67da
thekifake <thekidcorner@gmail.com>
2023-10-10 22:21:02 -0400

    convert json to js in bat file

--- get.bat
+++ get.bat
@@ -2,12 +2,17 @@
 if not exist "save_dir.txt" (
   set /p saveDir=Where should I send the JSON? 
 ) else (
-  set saveDir=<save_dir.txt
+  set /p saveDir=<save_dir.txt
 ) 
 echo %saveDir%> save_dir.txt
 echo  Checking for updates...
 cargo update
 echo  Building binaries and running...
 cargo run
-echo  Copying uiua_functions.json to the proper directory...
-copy uiua_functions.json %saveDir%
+echo  Converting the JSON to JS...
+echo const glyphs= >glyphs.js
+type uiua_functions.json>>glyphs.js
+echo  Copying...
+copy glyphs.js %saveDir%
+del glyphs.js
+echo  Done
